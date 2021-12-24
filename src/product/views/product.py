5975ede01253374b62f59b7ac10c13cf095f8660
nazmul1487 <nazmul15-1487@diu.edu.bd>
2021-12-24 22:27:16 +0600

    [Updated] Create Products   - data send from frontend. The Product create vue file is updated.   - save only the product table data from the request.
    resolves: #1

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,9 +1,13 @@
-from django.views import generic
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.generic import TemplateView
+from django.http import JsonResponse
+from product.models import Variant, Product, ProductVariant, ProductVariantPrice
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-from product.models import Variant
 
-
-class CreateProductView(generic.TemplateView):
+class CreateProductView(TemplateView):
     template_name = 'products/create.html'
 
     def get_context_data(self, **kwargs):
@@ -12,3 +16,23 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+
+class CreateProductAPIView(View):
+
+    @method_decorator(csrf_exempt, name="post")
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        data = json.loads(self.request.body)
+        product_data = {
+            "title": data.get("title", None),
+            "sku": data.get("sku", None),
+            "description": data.get("description", None),
+        }
+        a_product = Product.objects.create(**product_data)
+
+        return JsonResponse({"data": "New Product is created"})
+
+
--- src/templates/assets/js/components/product/CreateProduct.vue
+++ src/templates/assets/js/components/product/CreateProduct.vue
@@ -5,7 +5,7 @@
         <div class="card shadow mb-4">
           <div class="card-body">
             <div class="form-group">
-              <label for="">Product Name</label>
+              <label for="">Product Name:</label>
               <input type="text" v-model="product_name" placeholder="Product Name" class="form-control">
             </div>
             <div class="form-group">
@@ -129,7 +129,8 @@ export default {
         url: 'https://httpbin.org/post',
         thumbnailWidth: 150,
         maxFilesize: 0.5,
-        headers: {"My-Awesome-Header": "header value"}
+        headers: {"My-Awesome-Header": "header value"},
+        addRemoveLinks: true
       }
     }
   },
@@ -183,13 +184,11 @@ export default {
         title: this.product_name,
         sku: this.product_sku,
         description: this.description,
-        product_image: this.images,
+        product_image: this.$refs.myVueDropzone.getAcceptedFiles(),
         product_variant: this.product_variant,
         product_variant_prices: this.product_variant_prices
       }
-
-
-      axios.post('/product', product).then(response => {
+      axios.post('/product/', product).then(response => {
         console.log(response.data);
       }).catch(error => {
         console.log(error);
